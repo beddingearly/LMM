568d4cf8ae694b6e4b9748f8ef2f2cdbfd600163
beddingearly <wangzitongnowb@163.com>
2018-08-14 12:30:57 +0800

    0814

--- 70_Climbing_Stairs.py
+++ 70_Climbing_Stairs.py
@@ -5,3 +5,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        result = [1, 2]
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            for i in range(2, n):
+                result.append(result[i - 1] + result[i - 2])
+        return result[n - 1]
