a7b827a73db467b191c56b400d0e74c214e30a3c
beddingearly <wangzitongnowb@163.com>
2018-08-15 11:48:20 +0800

    0815

--- 100_Same_Tree.py
+++ 100_Same_Tree.py
@@ -18,8 +18,6 @@ class Solution(object):
         elif p == None or q == None:
             return False
         return q.val == p.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-
 if __name__ == '__main__':
     a = Solution()
     b = TreeNode(1)
