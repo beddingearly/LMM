8a390d8d26d9d37ffb560491ca16ddbb5f5d8522
beddingearly <wangzitongnowb@163.com>
2018-09-04 23:03:28 +0800

    0904

---
+++ 167_Two_Sum_II_Input_array_is_sorted.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        l = []
+        for i in range(len(numbers)):
+            if numbers[i] in d.keys():
+                return [d[numbers[i]] + 1, i + 1]
+            d[target - numbers[i]] = ibe
