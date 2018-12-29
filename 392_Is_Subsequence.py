49dd6cafa59a339dae8a7593394628c249ab9a91
beddingearly <wangzitongnowb@163.com>
2018-12-29 10:53:38 +0800

    20181229

--- 392_Is_Subsequence.py
+++ 392_Is_Subsequence.py
@@ -6,3 +6,24 @@
 @File    : 392_Is_Subsequence.py
 @Software: PyCharm
 '''
+
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == 0:
+            return True
+        if len(s) > len(t):
+            return False
+        index = 0
+        for i in range(len(t)):
+            if t[i] == s[index]:
+                index += 1
+                if index == len(s)-1:
+                    return True
+        return False
+
